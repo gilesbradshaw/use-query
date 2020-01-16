e6525aec20fa0d3ee153a5941aca6d2b66b06d8a
Giles <giles.bradshaw@sigyl.com>
2020-01-16 20:12:23 +0000

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
 
 import pkg from "./package.json";
 
@@ -30,29 +28,11 @@ export default {
   ],
   plugins: [
     external(),
-    resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
       exclude: "**/__tests__/**",
       clean: true
     }),
-    commonjs({
-      include: ["node_modules/**"],
-      namedExports: {
-        "node_modules/react/index.js": [
-          "useRef",
-          "useContext",
-          "useReducer",
-          "useEffect",
-          "useState",
-        ],
-        'node_modules/react-is/index.js': [
-          'isElement',
-          'isValidElementType',
-          'ForwardRef'
-        ]
-      }
-    })
   ],
   external: ['@apollo/react-hooks'],
 };
